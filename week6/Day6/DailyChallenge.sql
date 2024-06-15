2dd95634633d0865de693cd1aa91b6c1ac0f6797
Danil4UA <146213395+Danil4UA@users.noreply.github.com>
2024-06-15 12:44:58 +0300

    completed dailyChallenge

---
+++ week6/Day6/DailyChallenge.sql
@@ -0,0 +1,19 @@
+-- Database: Hollywood
+
+-- DROP DATABASE IF EXISTS "Hollywood";
+
+-- CREATE DATABASE "Hollywood"
+--     WITH
+--     OWNER = postgres
+--     ENCODING = 'UTF8'
+--     LC_COLLATE = 'en_US.UTF-8'
+--     LC_CTYPE = 'en_US.UTF-8'
+--     LOCALE_PROVIDER = 'libc'
+--     TABLESPACE = pg_default
+--     CONNECTION LIMIT = -1
+--     IS_TEMPLATE = False;
+
+SELECT COUNT(*) FROM actors;
+INSERT INTO actors(first_name, last_name)
+VALUES
+	('Tom', 'Cruz');
