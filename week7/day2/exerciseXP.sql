71a21f721df54030c69ed3a6f71facc31b640fd2
Danil4UA <146213395+Danil4UA@users.noreply.github.com>
2024-06-18 13:35:05 +0300

    did it in one file

--- week7/day2/exerciseXP.sql
+++ week7/day2/exerciseXP.sql
@@ -1,3 +1,19 @@
+-- Part 1
+
+SELECT * FROM items;
+
+-- All items, ordered by price (lowest to highest).
+SELECT * FROM items ORDER BY item_price;
+-- Items with a price above 80 (80 included), ordered by price (highest to lowest).
+SELECT * FROM items WHERE item_price >= 80 ORDER BY item_price DESC;
+-- The first 3 customers in alphabetical order of the first name (A-Z) – exclude the primary key column from the results.
+SELECT customer_name, customer_surname FROM customers ORDER BY customer_name ASC LIMIT 3;
+-- All last names (no other columns!), in reverse alphabetical order (Z-A)
+
+SELECT customer_surname FROM customers ORDER BY customer_surname DESC;
+
+-- Part2
+
 -- We will use the newly installed dvdrental database.
 
 -- In the dvdrental database write a query to select all the columns from the “customer” table.
