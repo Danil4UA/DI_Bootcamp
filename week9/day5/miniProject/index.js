7875a486b5e346bf749263b189489202cf0d34de
Danil4UA <146213395+Danil4UA@users.noreply.github.com>
2024-07-04 13:58:21 +0300

    created a basic logic and added some styles

---
+++ week9/day5/miniProject/index.js
@@ -0,0 +1,108 @@
+//Assets
+const robots = [
+    {
+      id: 1,
+      name: 'Leanne Graham',
+      username: 'Bret',
+      email: 'Sincere@april.biz',
+      image: 'https://robohash.org/1?200x200'
+    },
+    {
+      id: 2,
+      name: 'Ervin Howell',
+      username: 'Antonette',
+      email: 'Shanna@melissa.tv',
+      image: 'https://robohash.org/2?200x200'
+    },
+    {
+      id: 3,
+      name: 'Clementine Bauch',
+      username: 'Samantha',
+      email: 'Nathan@yesenia.net',
+      image: 'https://robohash.org/3?200x200'
+    },
+    {
+      id: 4,
+      name: 'Patricia Lebsack',
+      username: 'Karianne',
+      email: 'Julianne.OConner@kory.org',
+      image: 'https://robohash.org/4?200x200'
+    },
+    {
+      id: 5,
+      name: 'Chelsey Dietrich',
+      username: 'Kamren',
+      email: 'Lucio_Hettinger@annie.ca',
+      image: 'https://robohash.org/5?200x200'
+    },
+    {
+      id: 6,
+      name: 'Mrs. Dennis Schulist',
+      username: 'Leopoldo_Corkery',
+      email: 'Karley_Dach@jasper.info',
+      image: 'https://robohash.org/6?200x200'
+    },
+    {
+      id: 7,
+      name: 'Kurtis Weissnat',
+      username: 'Elwyn.Skiles',
+      email: 'Telly.Hoeger@billy.biz',
+      image: 'https://robohash.org/7?200x200'
+    },
+    {
+      id: 8,
+      name: 'Nicholas Runolfsdottir V',
+      username: 'Maxime_Nienow',
+      email: 'Sherwood@rosamond.me',
+      image: 'https://robohash.org/8?200x200'
+    },
+    {
+      id: 9,
+      name: 'Glenna Reichert',
+      username: 'Delphine',
+      email: 'Chaim_McDermott@dana.io',
+      image:'https://robohash.org/9?200x200'
+    },
+    {
+      id: 10,
+      name: 'Clementina DuBuque',
+      username: 'Moriah.Stanton',
+      email: 'Rey.Padberg@karina.biz',
+      image:'https://robohash.org/10?200x200'
+    }
+    ];
+
+
+//Elements
+const main = document.querySelector("main")
+const form = document.querySelector("form")
+const nameToSearch = document.getElementById("name-to-search")
+const search = document.getElementById("search-button")
+
+function createRobotsCards (arr){
+
+    const allCards = arr.map((item)=>{
+        return (
+            `<div id="card" class="card-${item.id}">
+                <img src="${item.image}"></img>
+                <p id="card-name">${item.name}</p>
+                <p id="card-email">${item.email}</p>
+            </div>`
+        )
+    })
+   return main.innerHTML = allCards.join("")
+}
+
+createRobotsCards(robots)
+
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    const userData = nameToSearch.value.toLowerCase().trim()
+    const filterRobots = robots.filter(item=>(item.name.toLowerCase()).includes(userData))
+    
+    main.innerHTML = ""
+    createRobotsCards(filterRobots)
+    console.log(filterRobots)
+
+})
