9b70b1a9aa0251f830e69d6fcd80fab528ea13db
Danil4UA <146213395+Danil4UA@users.noreply.github.com>
2024-07-16 19:15:49 +0300

    my commit

---
+++ week11/day2/DailyChallenge/app.js
@@ -0,0 +1,18 @@
+// common js 
+
+// const {greet, hello} = require("./greeting")
+// console.log(
+//     greet("Daniel")
+// )
+
+// ES6 - module 
+
+// import
+// export
+// export default
+
+import { greet, hello } from "./greeting.js"
+
+console.log(greet("aaa"));
+
+
---
+++ week11/day2/DailyChallenge/greeting.js
@@ -0,0 +1,17 @@
+export function greet (name){
+    return `Hello, ${name}`
+}
+
+export const hello = ()=>{
+    return `Hello`
+}
+
+
+// function greet (name){
+//     return `Hello, ${name}`
+// }
+
+// module.exports = {
+//     greet,
+// }
+
